49bf09f03131495e4c7f795fa1340b605784e832
yutiborn <yutib@webschool.co.il>
2022-01-10 18:09:24 +0200

    start project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello Mintaka
+     <h1>
+        אני לא מבינה כלום!!!!!!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
